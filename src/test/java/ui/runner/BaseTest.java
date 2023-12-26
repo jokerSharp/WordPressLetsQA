8897fac8c92693484c369d047b01c2e814f1f3b6
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2023-12-26 10:19:03 -0800

    BaseTest - waiters were added

--- src/test/java/ui/runner/BaseTest.java
+++ src/test/java/ui/runner/BaseTest.java
@@ -2,24 +2,26 @@ package ui.runner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.net.URL;
+import java.time.Duration;
 
 public abstract class BaseTest {
 
-    private static WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait2;
+    private WebDriverWait wait5;
+    private WebDriverWait wait10;
 
-    public static WebDriver getDriver() {
+    public WebDriver getDriver() {
         return driver;
     }
 
     @BeforeMethod
-    public static void setUp() {
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://localhost:8000/wp-admin/");
@@ -27,7 +29,28 @@ public abstract class BaseTest {
     }
 
     @AfterMethod
-    public static void teatDown() {
+    public void tearDown() {
         driver.quit();
+    }
+
+    protected WebDriverWait getWait2() {
+        if (wait2 == null) {
+            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        }
+        return wait2;
+    }
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+        return wait5;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (wait10 == null) {
+            wait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+        return wait10;
     }
 }
