7607dc632319f71ddfdde1c41c4dc7d751b81aa6
antari <spinogla@gmail.com>
2024-01-12 16:48:17 +0100

    add Header, add SidePanel, move tests

---
+++ src/test/java/ui/model/base/SidePanel.java
@@ -0,0 +1,27 @@
+package ui.model.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ui.model.users.UserPage;
+
+public class SidePanel {
+
+    private final WebDriver driver;
+
+    @FindBy(id = "menu-users")
+    private WebElement sideMenuUsersButton;
+
+    public SidePanel(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public UserPage goToUserPage() {
+        sideMenuUsersButton.click();
+
+        return new UserPage(driver);
+    }
+}
