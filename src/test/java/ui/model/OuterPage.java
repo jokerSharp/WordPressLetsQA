bf4160da5a51bc0519808b72fc7ed8703b07d674
InnaBaranova <135617336+InnaBaranova@users.noreply.github.com>
2024-02-08 20:14:59 +0100

    testClickMeetupsButton renamed to testOuterLinksWork and refactored (#56)

--- src/test/java/ui/model/OuterPage.java
+++ src/test/java/ui/model/OuterPage.java
@@ -1,16 +1,30 @@
 package ui.model;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import ui.model.base.BasePage;
 
 import java.util.ArrayList;
 
 public class OuterPage extends BasePage {
+
     public OuterPage(WebDriver driver) {
         super(driver);
     }
-    public String getOuterPageUrl() {
+
+    public OuterPage goToOuterPageTab() {
         ArrayList<String> tabs = new ArrayList<String>(getDriver().getWindowHandles());
-        return getDriver().switchTo().window(tabs.get(1)).getCurrentUrl();
+        getDriver().switchTo().window(tabs.get(1));
+
+        return new OuterPage(getDriver());
+    }
+
+    public DashboardPage closeOuterPageTabAndReturnToDashboardPage() {
+        ArrayList<String> tabs = new ArrayList<String>(getDriver().getWindowHandles());
+
+        getDriver().close();
+        getDriver().switchTo().window(tabs.get(0));
+
+        return new DashboardPage(getDriver());
     }
 }
