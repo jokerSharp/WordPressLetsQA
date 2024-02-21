196390d8008f9f24f8e66496f6a74040a6d82432
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-02-21 10:15:43 -0800

    testCreateComment() was expanded and refactored

--- src/test/java/ui/CommentTest.java
+++ src/test/java/ui/CommentTest.java
@@ -1,16 +1,18 @@
 package ui;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ui.model.DashboardPage;
 import ui.model.comments.CommentsPage;
+import ui.model.nonAdminPages.NAPostPage;
 import ui.runner.BaseTest;
 
 import java.util.List;
 
 public class CommentTest extends BaseTest {
 
-    private final String COMMENT = "new comment to first post";
+    private final String COMMENT = RandomStringUtils.randomAlphabetic(20);
 
     @Test
     public void testCreateComment() {
@@ -24,6 +26,14 @@ public class CommentTest extends BaseTest {
                 .getCommentsText();
 
         Assert.assertTrue(commentsList.contains(COMMENT));
+
+        List<String> commentsTextList = new NAPostPage(getDriver())
+                .goToDashboardPage()
+                .getSidePanel()
+                .clickSideMenuCommentsButton()
+                .getCommentsTextList();
+
+        Assert.assertTrue(commentsTextList.contains(COMMENT));
     }
 
     @Test(dependsOnMethods = "testCreateComment")
