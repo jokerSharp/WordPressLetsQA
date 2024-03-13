5c88e2a3563c665ab14867e9dfb8d13e5fee15ff
pavel <ppashchenkov@gmail.com>
2024-03-13 23:00:02 +0300

    Refactoring first ApiTest

--- src/test/java/ui/ApiTest.java
+++ src/test/java/ui/ApiTest.java
@@ -6,11 +6,7 @@ import org.testng.annotations.Test;
 import ui.model.DashboardPage;
 import ui.model.api.ApiPage;
 import ui.runner.BaseTest;
-import ui.model.api.ApiPage.*;
 import ui.runner.LoggerUtils;
-import ui.runner.ProjectUtils;
-
-import static io.restassured.RestAssured.given;
 
 public class ApiTest extends BaseTest {
 
@@ -18,7 +14,6 @@ public class ApiTest extends BaseTest {
     private final String SWAGGER = "swagger";
     private final String ACTIVATED = "Plugin activated.";
     private final String SWAGGER_API_DOCS_URL = "?page=swagger-ui";
-    private final String USERS_LIST = "/users?context=view&page=1&per_page=10&order=desc&orderby=id";
 
     @Test
     public void testPermalinksChange() {
@@ -62,12 +57,8 @@ public class ApiTest extends BaseTest {
     @Test(dependsOnMethods = "testPermalinksChange")
     public void getUsers() {
         String url = getDriver().getCurrentUrl().substring(0,23);
-        final String apiUrl = url + "wp-json/wp/v2";
+        Response users = ApiPage.getListUsers(url);
 
-        given().auth().preemptive().basic("admin", "Asdf.1234.qpwoei")
-                .when().get(apiUrl + USERS_LIST).then().statusCode(200);
-
-        Response users = ApiPage.getUserList(apiUrl + USERS_LIST);
         LoggerUtils.logInfo(users.asPrettyString());
 
         Assert.assertEquals(users.getStatusCode(), 200);
